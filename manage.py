bda6b44aa8caa518012f86ee7b04986c13ff5384
Alexandr Martynyuk <banifest@gmail.com>
2018-05-11 17:32:33 +0300

    bad test check

--- manage.py
+++ manage.py
@@ -18,19 +18,19 @@ if __name__ == "__main__":
             "forget to activate a virtual environment?"
         ) from exc
 
-    if 'do_test' in sys.argv:
-        a = subprocess.call('python manage.py test', shell=True)
-        print('\n\n\n\n\n\n\n\n\n\n\n')
-        print(a)
-        if a == 1:
-            raise OperationalError()
-        else:
-            sys.exit(0)
-
-    if 'test' in sys.argv:
-        try:
-            execute_from_command_line(sys.argv)
-        except OperationalError:
-            pass
-    else:
-        execute_from_command_line(sys.argv)
+    # if 'do_test' in sys.argv:
+    #     a = subprocess.call('python manage.py test', shell=True)
+    #     print('\n\n\n\n\n\n\n\n\n\n\n')
+    #     print(a)
+    #     if a == 1:
+    #         raise OperationalError()
+    #     else:
+    #         sys.exit(0)
+    #
+    # if 'test' in sys.argv:
+    #     try:
+    #         execute_from_command_line(sys.argv)
+    #     except OperationalError as E:
+    #         pass
+    # else:
+    execute_from_command_line(sys.argv)
