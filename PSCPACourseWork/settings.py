e2801cff35ad66a303a02012c6b7596ebaacea72
Alexandr Martynyuk <banifest@gmail.com>
2018-05-11 11:11:38 +0300

    bad test check

--- PSCPACourseWork/settings.py
+++ PSCPACourseWork/settings.py
@@ -114,7 +114,7 @@ if 'test' in sys.argv:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'databasename.db3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
