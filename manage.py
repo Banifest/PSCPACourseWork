a62a97b0c6e615108e997d1b817c503da9687f92
Alexandr Martynyuk <banifest@gmail.com>
2018-05-11 11:18:57 +0300

    bad test check

--- manage.py
+++ manage.py
@@ -12,4 +12,11 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+
+    if 'test' in sys.argv:
+        try:
+            execute_from_command_line(sys.argv)
+        except:
+            pass
+    else:
+        execute_from_command_line(sys.argv)
