356529ee3062a8cac8b4f6531fd83f6ab217dcc3
Alexandr Martynyuk <banifest@gmail.com>
2018-05-12 09:56:42 +0300

    good test check

--- PSCPACourseWork/settings.py
+++ PSCPACourseWork/settings.py
@@ -110,10 +110,14 @@ print(sys.argv)
 
 if 'test' in sys.argv:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'TestPostgres',
+                'USER': 'postgres',
+                'PASSWORD': 'admin',
+                'HOST': '127.0.0.1',
+                'PORT': '5432',
+            }
     }
 
 
